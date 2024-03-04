51ec164f63dbab43ea95fdb86c6722fe4509cfa1
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-04 21:25:02 +0200

    created songInfo ui

---
+++ apps/nx-spotify/src/widgets/footer/song-info/song-info.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { HeartIcon } from "@spotify/icons";
+import { Button, Link, animations, cn, useOptionalStyle } from "@spotify/shared";
+import { FC, useState } from "react";
+
+export const SongInfo: FC = () => {
+  const [liked, setLike] = useState(false);
+  const { className, enableStyle, disableStyle } = useOptionalStyle({
+    style: animations.shake,
+    timing: "SHORT",
+  });
+  return (
+    <div className="flex items-center gap-3 text-nowrap">
+      <div className="size-[60px] rounded-md bg-black-150"></div>
+      <div className="flex flex-col">
+        <Link href={"/"} className="hover:underline">
+          redrum
+        </Link>
+        <Link href={"/"} className="text-black-150 hover:text-white-1000 hover:underline">
+          21 Savage
+        </Link>
+      </div>
+      <Button
+        onClick={() => {
+          setLike((prev) => !prev);
+          liked ? disableStyle() : enableStyle();
+        }}
+      >
+        <HeartIcon
+          className={cn(
+            "ml-2 transition-colors",
+            liked ? "fill-green-800 stroke-green-800" : "hover:stroke-white-1000",
+            className,
+          )}
+        />
+      </Button>
+    </div>
+  );
+};
