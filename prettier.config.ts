f6acea5b567c94516098ff975955b4015d922b81
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-04 21:00:29 +0200

    configured prettier and eslint

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+
+module.exports = {
+  "tabWidth": 2,
+  "printWidth": 120,
+  "semi": true,
+  "arrowParens": "always",
+  "trailingComma": "all",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "singleQuote": false,
+  plugins: [import('prettier-plugin-tailwindcss')],
+}
+
