60f76af62d639532826932e9c13f9b837a797c47
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-06 11:56:53 +0200

    created user service

---
+++ apps/nx-express-spotify/src/services/user-service.ts
@@ -0,0 +1,59 @@
+import bcrypt from "bcrypt";
+import argon2 from "argon2";
+import { tokenService } from "./token-service";
+const saltRounds = 42;
+
+const {
+  deleteToken,
+  generateToken,
+  saveToken,
+  validateAccessToken,
+  validateRefreshToken,
+} = tokenService();
+
+export const userService = () => {
+  const signup = async ({
+    email,
+    password,
+    userName,
+  }: {
+    email: string;
+    password: string;
+    userName: string;
+  }) => {
+    try {
+      const hashedPassword = await argon2.hash(password);
+    } catch (err) {
+      //...
+    }
+    const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+    /// add user to db email unique , hashedPassword, userName
+
+    const tokens = generateToken({
+      email,
+      userName,
+    });
+
+    return { ...tokens, user: { email, userName } };
+  };
+  const login = async ({
+    password,
+    email,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const hashedPassword = "hashedPAssword"; /// user hashed password form db
+      if (!(await argon2.verify(hashedPassword, password))) "error"; /// error if password didn`t matches
+    } catch (error) {
+      //..
+    }
+  };
+  const logout = async (refreshToken: string) => {
+    deleteToken(refreshToken);
+  };
+
+  return { logout, login, signup };
+};
