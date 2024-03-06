289b013ce86fc2dbb126d5ddbcf039b95b3dde6a
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-06 11:53:47 +0200

    created user controller

---
+++ apps/nx-express-spotify/src/controllers/user-controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+
+export const userController = () => {
+  const login = (res: Response, req: Request) => {
+    try {
+      ("");
+    } catch (error) {
+      ("");
+    }
+  };
+  const signup = (res: Response, req: Request) => {
+    try {
+      ("");
+    } catch (error) {
+      ("");
+    }
+  };
+  const refresh = (res: Response, req: Request) => {
+    try {
+      ("");
+    } catch (error) {
+      ("");
+    }
+  };
+  const logout = (res: Response, req: Request) => {
+    try {
+      ("");
+    } catch (error) {
+      ("");
+    }
+  };
+  return { login, signup, refresh, logout };
+};
