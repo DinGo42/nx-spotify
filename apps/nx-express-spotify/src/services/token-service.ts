f9ab17b52a42ecd13fd251035e99d323e4388912
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-06 11:56:46 +0200

    created token service

---
+++ apps/nx-express-spotify/src/services/token-service.ts
@@ -0,0 +1,48 @@
+import jwt from "jsonwebtoken";
+
+export const tokenService = () => {
+  const generateToken = (payload: object | string) => {
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET_KEY, {
+      expiresIn: "30d",
+    });
+
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET_KEY, {
+      expiresIn: "30m",
+    });
+    return { refreshToken, accessToken };
+  };
+
+  const validateRefreshToken = (token: string) => {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET_KEY);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  };
+
+  const validateAccessToken = (token: string) => {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET_KEY);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  };
+
+  const saveToken = (token: string) => {
+    ///save token to cookie
+  };
+
+  const deleteToken = (token: string) => {
+    ///delete this token from cookie
+  };
+
+  return {
+    generateToken,
+    saveToken,
+    deleteToken,
+    validateRefreshToken,
+    validateAccessToken,
+  };
+};
