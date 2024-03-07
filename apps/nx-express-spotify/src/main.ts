024dc14946c834369f6e197a4cc22151a0132490
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-07 22:10:25 +0200

    added error middleware

--- apps/nx-express-spotify/src/main.ts
+++ apps/nx-express-spotify/src/main.ts
@@ -1,37 +1,20 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares";
 import { authRouter } from "./routes";
 
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-// const test = async () =>
-//   await prisma.users.create({
-//     data: {
-//       email: "ivan.nesterenko@ukr.net",
-//       password: "12345",
-//       userName: "Imaizumi",
-//     },
-//   });
-
-// test();
-
 dotenv.config();
 
 const app = express();
-app.use(errorMiddleware);
-
+app.use(json());
 app.use(cors());
 app.use(cookieParser());
 
-app.use("./auth", authRouter);
-app.get("/api", (req, res) => {
-  res.send({ message: "Welcome to nx-express-spotify!" });
-});
+app.use("/auth", authRouter);
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
