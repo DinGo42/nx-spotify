be59cde346f80349dd1e4d5ad665d89b42e0d0b7
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-07 22:13:43 +0200

    deleted unnecessary routes

--- apps/nx-express-spotify/src/routes/auth.ts
+++ apps/nx-express-spotify/src/routes/auth.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { userController } from "../controllers";
+import { STATUS_CODES } from "../exceptions";
+import { authMiddleware } from "../middlewares";
 
-const { login, logout, refresh, signup } = userController();
+const { login, logout, signup } = userController();
 export const authRouter = Router();
 
-authRouter.post("./login", login);
-authRouter.post("./logout", logout);
-authRouter.post("./signup", refresh);
-authRouter.get("./refresh", signup);
+authRouter.post("/signup", signup);
+authRouter.post("/login", login);
+authRouter.get("/logout", logout);
+authRouter.get("/users", authMiddleware, (req, res) => {
+  res.status(STATUS_CODES.SUCCESS).json({ message: "Success u authorized" });
+});
