c847448706e3374c30328ef89c0b1918953dc4fd
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-07 22:10:40 +0200

    added logic to user controller

--- apps/nx-express-spotify/src/controllers/user-controller.ts
+++ apps/nx-express-spotify/src/controllers/user-controller.ts
@@ -1,33 +1,54 @@
 import { Request, Response } from "express";
+import { userService } from "../services";
+import { STATUS_CODES } from "../exceptions";
+
+const { login: loginService, signup: signupService } = userService();
 
 export const userController = () => {
-  const login = (res: Response, req: Request) => {
+  const signup = async (req: Request, res: Response, next) => {
     try {
-      ("");
+      const { email, password, userName } = req.body;
+      const { accessToken, refreshToken, user } = await signupService({
+        email,
+        password,
+        userName,
+      });
+      console.log("refreshToken");
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 900000,
+      });
+      if (user) res.status(STATUS_CODES.SUCCESS).json({ message: "Logined" });
     } catch (error) {
-      ("");
+      next(error);
     }
   };
-  const signup = (res: Response, req: Request) => {
+
+  const login = async (req: Request, res: Response, next) => {
     try {
-      ("");
+      const { email, password } = req.body;
+
+      const result = await loginService({
+        email,
+        password,
+      });
+
+      res.cookie("refreshToken", result.refreshToken, {
+        httpOnly: true,
+        maxAge: 90000,
+      });
+      if (result) res.status(STATUS_CODES.SUCCESS).json({ message: "Logined" });
     } catch (error) {
-      ("");
+      next(error);
     }
   };
-  const refresh = (res: Response, req: Request) => {
-    try {
-      ("");
-    } catch (error) {
-      ("");
-    }
+
+  const logout = (req: Request, res: Response) => {
+    res.cookie("refreshToken", "", {
+      httpOnly: true,
+      maxAge: 0,
+    });
+    res.status(STATUS_CODES.SUCCESS).json({ message: "Successfully logout" });
   };
-  const logout = (res: Response, req: Request) => {
-    try {
-      ("");
-    } catch (error) {
-      ("");
-    }
-  };
-  return { login, signup, refresh, logout };
+  return { login, signup, logout };
 };
