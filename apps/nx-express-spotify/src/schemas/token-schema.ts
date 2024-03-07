9ffc2eb58ebce22fe1b6d08a5c1e96780f6b9535
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-07 22:14:28 +0200

    deleted schemas

--- apps/nx-express-spotify/src/schemas/token-schema.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod";
-
-export const tokenSchema = z.object({
-  user: z.string().uuid(),
-  refreshToken: z.string(),
-});
--- apps/nx-express-spotify/src/schemas/user-schema.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-import validator from "validator";
-
-export const userSchema = z.object({
-  userName: z.string(),
-  password: z.string().min(4),
-  email: z.string().email().refine(validator.isEmail),
-});
