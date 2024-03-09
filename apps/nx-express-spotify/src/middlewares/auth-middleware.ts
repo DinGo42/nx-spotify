e3565bd493df16bf1a886c96d0c6e9a0da8e93a6
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-09 20:32:34 +0200

    finalized auth middleware

--- apps/nx-express-spotify/src/middlewares/auth-middleware.ts
+++ apps/nx-express-spotify/src/middlewares/auth-middleware.ts
@@ -2,17 +2,30 @@ import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../exceptions";
 import { tokenService } from "../services";
 
-const { validateRefreshToken } = tokenService();
+const { refreshTokens, saveTokens } = tokenService({});
 
 export const authMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const token = req.cookies.refreshToken;
+  const refreshToken: string = req.cookies.refreshToken;
+  const accessToken: string = req.cookies.accessToken.split(" ")[1];
 
-  if (!token) return next(ApiError.UnauthorizedError());
+  if (!refreshToken) return next(ApiError.UnauthorizedError(`Session expired`));
 
-  if (!validateRefreshToken(token)) next(ApiError.UnauthorizedError());
+  const newTokens = refreshTokens({
+    accessToken,
+    refreshToken,
+  });
+
+  if (!newTokens) return next(ApiError.UnauthorizedError(`Session expired`));
+
+  saveTokens({
+    res,
+    accessToken: newTokens.accessToken,
+    refreshToken: newTokens.refreshToken,
+  });
+
   next();
 };
