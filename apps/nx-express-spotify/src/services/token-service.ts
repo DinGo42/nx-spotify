b39669e4c056d6ee5142c7a5a04e90d13bfb2ecf
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-03-10 00:03:52 +0200

    updated tokenService

--- apps/nx-express-spotify/src/services/token-service.ts
+++ apps/nx-express-spotify/src/services/token-service.ts
@@ -1,37 +1,100 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Response } from "express";
 
-export const tokenService = () => {
+type TokenServiceProps = {
+  accessTokenMaxTime?: number;
+  refreshTokenMaxTime?: number;
+};
+
+const accessTokenMaxTime = 1800000;
+const refreshTokenMaxTime = 2592000000;
+
+export const tokenService = (tokensMaxTime?: TokenServiceProps) => {
   const generateToken = (payload: object | string) => {
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET_KEY, {
-      expiresIn: "30d",
+      expiresIn: tokensMaxTime?.refreshTokenMaxTime || refreshTokenMaxTime,
     });
 
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET_KEY, {
-      expiresIn: "30m",
+      expiresIn: tokensMaxTime?.accessTokenMaxTime || accessTokenMaxTime,
     });
     return { refreshToken, accessToken };
   };
 
-  const validateRefreshToken = (token: string) => {
-    try {
-      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET_KEY);
-      return userData;
-    } catch (e) {
-      return null;
-    }
+  const validateRefreshToken = (token: string | null): JwtPayload | null => {
+    if (!token) return null;
+    const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET_KEY);
+    if (typeof userData === "string") return null;
+    return userData;
   };
 
-  const validateAccessToken = (token: string) => {
-    try {
-      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET_KEY);
-      return userData;
-    } catch (e) {
-      return null;
-    }
+  const validateAccessToken = (token: string | null) => {
+    if (!token) return null;
+    const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET_KEY);
+    if (typeof userData === "string") return null;
+    return userData;
+  };
+
+  const refreshTokens = ({
+    accessToken,
+    refreshToken,
+  }: {
+    accessToken: string | null;
+    refreshToken: string | null;
+  }) => {
+    const accessTokenCheck = validateAccessToken(accessToken);
+
+    if (accessTokenCheck && accessToken && refreshToken)
+      return { accessToken, refreshToken };
+
+    const checkResult = validateRefreshToken(refreshToken);
+
+    if (!checkResult) return null;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { aud, exp, iat, iss, jti, nbf, sub, ...userData } = checkResult;
+
+    const newTokens = generateToken(userData);
+
+    return newTokens;
+  };
+
+  const saveTokens = ({
+    accessToken,
+    refreshToken,
+    res,
+  }: {
+    res: Response;
+    accessToken: string;
+    refreshToken: string;
+  }) => {
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: tokensMaxTime?.refreshTokenMaxTime || refreshTokenMaxTime,
+    });
+    res.cookie("accessToken", `Bearer ${accessToken}`, {
+      httpOnly: true,
+      maxAge: tokensMaxTime?.accessTokenMaxTime || accessTokenMaxTime,
+    });
+  };
+
+  const deleteTokens = (res: Response) => {
+    res.cookie("refreshToken", "", {
+      httpOnly: true,
+      maxAge: 0,
+    });
+
+    res.cookie("accessToken", "", {
+      httpOnly: true,
+      maxAge: 0,
+    });
   };
   return {
     generateToken,
     validateRefreshToken,
     validateAccessToken,
+    refreshTokens,
+    saveTokens,
+    deleteTokens,
   };
 };
