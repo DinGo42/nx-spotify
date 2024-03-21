ebb9d5ce8c81998cde9f6e349212c97fcf0099b7
Imaizumi <ivan.nesterenko@ukr.net>
2024-03-21 11:23:14 +0200

    changed inner logic for auth service

--- apps/express-backend/src/services/auth-service.ts
+++ apps/express-backend/src/services/auth-service.ts
@@ -1,14 +1,13 @@
 import argon2 from "argon2";
 import { PrismaClient } from "@bd";
-import { ForbiddenError, NotFoundError, UnauthorizedError, tokenService } from "@shared";
+import { ForbiddenError, NotFoundError, UnauthorizedError } from "@shared";
+import { generateToken } from "../main";
 
 const prisma = new PrismaClient();
 
-const { generateToken } = tokenService();
-
 export const authService = () => {
-  const signup = async ({ email, password, userName }: { email: string; password: string; userName: string }) => {
-    const emailCheck = await prisma.users.findUnique({
+  const signup = async ({ email, password, nickname }: { email: string; password: string; nickname: string }) => {
+    const emailCheck = await prisma.user.findUnique({
       where: {
         email,
       },
@@ -19,25 +18,25 @@ export const authService = () => {
 
     if (emailCheck) throw new ForbiddenError(`User with email:${email} already exist`);
 
-    const tokens = generateToken({
-      email,
-      userName,
-    });
-
     const hashedPassword = await argon2.hash(password);
 
-    await prisma.users.create({
+    const { id } = await prisma.user.create({
       data: {
         email,
         password: hashedPassword,
-        userName: userName,
+        nickname: nickname,
+      },
+      select: {
+        id: true,
       },
     });
 
-    return { ...tokens, user: { email, userName } };
+    const tokens = generateToken(id);
+
+    return tokens;
   };
   const login = async ({ password, email }: { email: string; password: string }) => {
-    const user = await prisma.users.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         email,
       },
@@ -45,18 +44,13 @@ export const authService = () => {
 
     if (!user) throw new NotFoundError(`User with email:${email} not found`);
 
-    const { password: hashedPassword, ...userDto } = user;
+    const { password: hashedPassword, id } = user;
 
     if (!(await argon2.verify(hashedPassword, password))) throw new UnauthorizedError(`Password doesn't match`);
 
-    const tokens = generateToken({
-      ...userDto,
-    });
+    const tokens = generateToken(id);
 
-    return {
-      ...tokens,
-      ...userDto,
-    };
+    return tokens;
   };
 
   return { login, signup };
