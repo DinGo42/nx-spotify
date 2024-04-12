76907c024280a1a4caaf9dd8f06c526df4ae1930
Imaizumi <ivan.nesterenko@ukr.net>
2024-04-12 10:48:09 +0300

    updated prisma module

--- apps/express-backend/src/modules/prisma/index.ts
+++ apps/express-backend/src/modules/prisma/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-export { ListeningHistory, Playlist, Prisma, User } from "@prisma/client";
+export { Prisma } from "@prisma/client";
+export type { ListeningHistory, Playlist, User } from "@prisma/client";
 
 export const prisma = new PrismaClient();
