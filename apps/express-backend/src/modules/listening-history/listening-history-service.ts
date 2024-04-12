cb0802283316031e45a71a2e8fa9f0e6577d0be3
Imaizumi <ivan.nesterenko@ukr.net>
2024-04-12 10:50:26 +0300

    fixed types

--- apps/express-backend/src/modules/listening-history/listening-history-service.ts
+++ apps/express-backend/src/modules/listening-history/listening-history-service.ts
@@ -1,9 +1,13 @@
 import { Prisma, prisma } from "../prisma";
 
-export const createListeningHistoryService = async (create: Prisma.ListeningHistoryCreateManyArgs) => {
+export const createListeningHistoryService = async <T extends Prisma.ListeningHistoryCreateManyArgs>(
+  create: Prisma.SelectSubset<T, Prisma.ListeningHistoryCreateManyArgs>,
+) => {
   return await prisma.listeningHistory.createMany(create);
 };
 
-export const getListeningHistoryService = async (get: Prisma.ListeningHistoryFindManyArgs) => {
+export const getListeningHistoryService = async <T extends Prisma.ListeningHistoryFindManyArgs>(
+  get: Prisma.SelectSubset<T, Prisma.ListeningHistoryFindManyArgs>,
+) => {
   return await prisma.listeningHistory.findMany(get);
 };
--- apps/express-backend/src/modules/user/user-service.ts
+++ apps/express-backend/src/modules/user/user-service.ts
@@ -12,12 +12,16 @@ export const getSelfService = async <T extends Prisma.UserFindUniqueArgs>(
   return user;
 };
 
-export const updateAccountService = async (update: Prisma.UserUpdateArgs) => {
+export const updateAccountService = async <T extends Prisma.UserUpdateArgs>(
+  update: Prisma.SelectSubset<T, Prisma.UserUpdateArgs>,
+) => {
   const user = await prisma.user.update(update);
   if (!user) throw new NotFoundError(`Failed to get information. User not found`);
 };
 
-export const deleteAccountService = async (data: Prisma.UserDeleteArgs) => {
+export const deleteAccountService = async <T extends Prisma.UserDeleteArgs>(
+  data: Prisma.SelectSubset<T, Prisma.UserDeleteArgs>,
+) => {
   const user = await prisma.user.delete(data);
   if (!user) throw new NotFoundError(`Failed to get information. User not found`);
   return user;
